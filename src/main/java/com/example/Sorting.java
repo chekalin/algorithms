2e88205c970840468031ef7aed91510eeb24c56c
Stanislav Chekalin <Stanislavs.Cekalins@Capita.co.uk>
2015-12-24 11:38:41 +0000

    bubble optimization: ignoring elements at the back of the array that are already sorted

--- src/main/java/com/example/Sorting.java
+++ src/main/java/com/example/Sorting.java
@@ -4,14 +4,16 @@ public class Sorting {
 
     public static int[] bubbleSort(int[] array) {
         boolean swapped;
+        int sortedItemsAtTheBack = 0;
         do {
             swapped = false;
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < array.length - 1 - sortedItemsAtTheBack; i++) {
                 if (array[i] > array[i + 1]) {
                     swap(array, i, i + 1);
                     swapped = true;
                 }
             }
+            sortedItemsAtTheBack++;
         } while (swapped);
         return array;
     }
